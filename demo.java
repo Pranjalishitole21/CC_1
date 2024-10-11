b0071ae84a864a060987a74062f3103278b64e70
Pranjalishitole21 <118060702+Pranjalishitole21@users.noreply.github.com>
2024-10-11 10:40:50 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,9 @@
+
+
+class demo {
+    public static void main(String[] args) {
+        System.out.println("This program is generated on GitHub Platform"); 
+        System.out.println("This JAVA program is exceuted with Google App Engine");
+      
+    }
+}
