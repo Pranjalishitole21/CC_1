f5218cd2306bc7188b7ef79ce8c8dce8bb80c880
Pranjalishitole21 <118060702+Pranjalishitole21@users.noreply.github.com>
2024-10-11 10:35:12 +0530

    Update demo.c

--- demo.c
+++ demo.c
@@ -2,6 +2,6 @@
 int main() {
   
    printf("\n\nThis program is generated on GitHub Platform\n\n");
-printf("\n\nThis C program is exceuted with Google App Enginen\n\n");
+printf("\n\nThis C program is exceuted with Google App Engine\n\n");
    return 0;
 }
